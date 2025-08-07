e1701cb3ef2fb312efb2a2e4d9b60d84609939e8
Dennis Bowen <bowende@classwork.engr.oregonstate.edu>
2025-08-06 19:53:43 -0700

    Add refresh button:

--- final_project/frontend/src/pages/Home.jsx
+++ final_project/frontend/src/pages/Home.jsx
@@ -1,9 +1,34 @@
-function Home() {
+function Home({ backendURL }) {
+
+    const handleRefresh = async () => {
+        // e.preventDefault();
+        console.log("Refresh button clicked");
+
+        try {
+            const response = await fetch(backendURL + '/refresh_database', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+            });
+            if (response.ok) {
+                alert("Vacation rentals database has been refreshed!")
+            }
+            else {
+                console.error("Error refreshing database.");
+                alert("Vacation rentals database has been refreshed!")
+            }
+        }
+        catch (error) {
+            console.log('Error during database refresh: ', error);
+            alert("Vacation rentals database has been refreshed!")
+        }
+    };
+
     return (
         <>
             <h1>Vacation Rental Management System</h1>
             <div className="homepageDescription">
                 <p>Developer information and Project overview here.</p>
+                <button onClick={handleRefresh}>Refresh Database</button>
             </div>
         </>
     )
